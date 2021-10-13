ac49089de5fecd43c44cf5b0b4eea2820a71dd3b
haya <haya.saud.ruh.k@tuwaiq.edu.sa>
2021-10-13 23:42:08 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "q2function_without_return"
+
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun printstudentname(){
+    
+    println("haya")
+    println("raghad")
+    println("afrah")
+    println("fouzeyah")
+    println("nourah")
+    println("reem")
+    println("areej")
+    println("hessah")
+    println("amal")
+
+}
+fun main(args: Array<String>) {
+    println("name of new students:")
+    var name = printstudentname()
+    println( )
+}
